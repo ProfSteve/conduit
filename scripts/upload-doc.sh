d867d5fdcca417264f8dbaea354d11c451812d66
John Stowers <jstowers@src.gnome.org>
2008-04-30 09:15:32 +0000

    Remove wrongly tagged release
    svn path=/tags/0.3.10; revision=1435

--- scripts/upload-doc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-APP="conduit/conduit"
-if [ ! -f $APP ] ; then
-    echo "ERROR: Must be run from top directory"
-    exit 1
-fi
-
-rsync -qtz doc/*.{html,gif,png,py,js,css} root@greenbirdsystems.com:/var/www/conduit-project.org/doc/conduit
--- trunk/conduit/dataproviders/DataProviderCategory.py
+++
@@ -1,5 +0,0 @@
-class DataProviderCategory:
-    def __init__(self, name, icon="image-missing", key=""):
-        self.name = name
-        self.icon = icon
-        self.key = name + key
