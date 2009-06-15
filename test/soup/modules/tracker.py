eba789e2af9cc3610f64d8a6dfb88672f65ebaa9
John Carr <john.carr@unrouted.co.uk>
2009-06-15 05:16:16 +0100

    Make sure tracker is empty before we run tests

--- test/soup/modules/tracker.py
+++ test/soup/modules/tracker.py
@@ -7,12 +7,17 @@ from soup.data.event import EventWrapper
 
 import conduit.modules.TrackerModule.TrackerModule as TrackerModule
 
+import tralchemy
+from tralchemy import nco, ncal
+
 class TrackerContacts(soup.modules.ModuleWrapper):
 
     klass = TrackerModule.TrackerContacts
     dataclass = ContactWrapper
 
     def create_dataprovider(self):
+        for contact in nco.PersonContact.get():
+            contact.delete()
         return self.klass()
 
     def destroy_dataprovider(self):
@@ -24,6 +29,8 @@ class TrackerCalendar(soup.modules.ModuleWrapper):
     dataclass = EventWrapper
 
     def create_dataprovider(self):
+        for event in ncal.Event.get():
+            event.delete()
         return self.klass()
 
     def destroy_dataprovider(self):
