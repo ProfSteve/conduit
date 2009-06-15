20e57d205df8852e94e6930c7b9628575440c1ac
John Carr <john.carr@unrouted.co.uk>
2009-06-15 14:35:28 +0100

    Map phone number and email address

--- conduit/modules/TrackerModule/TrackerModule.py
+++ conduit/modules/TrackerModule/TrackerModule.py
@@ -76,7 +76,8 @@ class TrackerContacts(DataProvider.TwoWay):
             if k == "account":
                 pass
             elif k == "tel":
-                pass
+                pn = nco.PhoneNumber(phonenumber=v[0].value)
+                c.hasphonenumber = pn
             elif k == "bday":
                 c.birthdate = v[0].value
             elif k == "n":
@@ -110,7 +111,8 @@ class TrackerContacts(DataProvider.TwoWay):
             elif k == "nickname":
                 c.nickname = v[0].value
             elif k == "email":
-                pass
+                ea = nco.EmailAddress(emailaddress=v[0].value)
+                c.hasemailaddress = ea
             elif k == "fn":
                 c.fullname = v[0].value
             elif k == "x-gender":
