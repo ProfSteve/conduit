ffd23af2c46ca8af1ab5d06a2d37cb64dce438f7
Alexandre Rosenfeld <airmind@gmail.com>
2009-08-16 15:22:05 -0300

    Fixed problems merging master

--- conduit/dataproviders/__init__.py
+++ conduit/dataproviders/__init__.py
@@ -14,13 +14,13 @@ CATEGORY_BOOKMARKS = 'CATEGORY_BOOKMARKS'
 CATEGORY_TEST = 'CATEGORY_TEST'
 
 CATEGORIES = {
-    CATEGORY_FILES = DataProviderCategory.DataProviderCategory(N_("Files and Folders"), "computer")
-    CATEGORY_NOTES = DataProviderCategory.DataProviderCategory(N_("Notes"), "tomboy")
-    CATEGORY_PHOTOS = DataProviderCategory.DataProviderCategory(N_("Photos"), "image-x-generic")
-    CATEGORY_OFFICE = DataProviderCategory.DataProviderCategory(N_("Office"), "applications-office")
-    CATEGORY_SETTINGS = DataProviderCategory.DataProviderCategory(N_("Settings"), "applications-system")
-    CATEGORY_MISC = DataProviderCategory.DataProviderCategory(N_("Miscellaneous"), "applications-accessories")
-    CATEGORY_MEDIA = DataProviderCategory.DataProviderCategory(N_("Media"), "applications-multimedia")
-    CATEGORY_BOOKMARKS = DataProviderCategory.DataProviderCategory(N_("Bookmarks"), "user-bookmarks")
-    CATEGORY_TEST = DataProviderCategory.DataProviderCategory(N_("Test"))
+    CATEGORY_FILES : DataProviderCategory.DataProviderCategory(N_("Files and Folders"), "computer"),
+    CATEGORY_NOTES : DataProviderCategory.DataProviderCategory(N_("Notes"), "tomboy"),
+    CATEGORY_PHOTOS : DataProviderCategory.DataProviderCategory(N_("Photos"), "image-x-generic"),
+    CATEGORY_OFFICE : DataProviderCategory.DataProviderCategory(N_("Office"), "applications-office"),
+    CATEGORY_SETTINGS : DataProviderCategory.DataProviderCategory(N_("Settings"), "applications-system"),
+    CATEGORY_MISC : DataProviderCategory.DataProviderCategory(N_("Miscellaneous"), "applications-accessories"),
+    CATEGORY_MEDIA : DataProviderCategory.DataProviderCategory(N_("Media"), "applications-multimedia"),
+    CATEGORY_BOOKMARKS : DataProviderCategory.DataProviderCategory(N_("Bookmarks"), "user-bookmarks"),
+    CATEGORY_TEST : DataProviderCategory.DataProviderCategory(N_("Test")),
 }
